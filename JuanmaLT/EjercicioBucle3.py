bdfd9b15dccd77d8133c21bc26c0a6c90d5460d0
JuanmaaLopez02 <lopeztorreesjuanma@gmail.com>
2025-02-21 09:27:21 +0100

    Adding even and odd exercises

---
+++ JuanmaLT/EjercicioBucle3.py
@@ -0,0 +1,2 @@
+#Autor: Juan Manuel López Torres
+#Algoritmo que pida números hasta que se introduzca un cero. Debe imprimir la suma y la media de todos los números introducidos.
---
+++ JuanmaLT/EjercicioBucle6.py
@@ -0,0 +1,5 @@
+#Autor: Juan Manuel López Torres
+#Escribir un programa que imprima todos los números pares entre dos números que se le pidan al usuario
+
+num1=int(input("Indique el primer numero: "))
+num2=int(input("Indique el segundo numero"))
