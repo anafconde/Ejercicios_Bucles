39cfa4e17d13e21c6397be9b2cc9b2d6cc2156c6
Hector20062006 <hrp7511@gmail.com>
2025-02-22 18:58:19 +0100

    I upload excersice 12

--- Hector_RP/Ejercicio_12.py
+++ Hector_RP/Ejercicio_12.py
@@ -7,6 +7,6 @@ for mes in range(1, 13):
     deposito = int(input(f"Ingrese la cantidad depositada en el mes {mes}: "))
     ahorros_totales += deposito
     ahorros_mensuales.append(ahorros_totales)
-    print(f"Ahorros acumulados hasta el mes {mes}: {ahorros_totales:.2f} euros")
+    print(f"Ahorros acumulados hasta el mes {mes}: {ahorros_totales} euros")
 
-print(f"\nEl total ahorrado en el año es: {ahorros_totales:.2f} euros")
+print(f"El total ahorrado en el año es: {ahorros_totales} euros")
