055f72eb26a42ef38eb95ecfcdf7d954914037d4
Laura-Linares <lauralinarescontacto@gmail.com>
2025-02-26 12:33:57 +0100

    Added code in exercise 18

--- LauraLL/Ejercicio18.py
+++ LauraLL/Ejercicio18.py
@@ -1,10 +1,11 @@
 #-------Autor:LauraLinares-------
-#-----------Version:V1-----------
+#-----------Version:V1.1-----------
 #-----Enunciado del ejercicio----
 # Hacer un programa que muestre un cronómetro, indicando las horas, minutos y segundos
 
 #----Importador de bibliotecas---
 import os
+import time
 
 #---Inicializador de variables---
 
@@ -15,4 +16,6 @@ os.system("cls")
 for h in range(0,61):
     for min in range(0,61):
         for seg in range(0,61):
+            os.system("cls")
             print(f"{h}:{min}:{seg}")
+            time.sleep(1)
